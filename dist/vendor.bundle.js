0dea4886a38df6afcb97eb76d4c807547ae33734
Cole R Lawrence <Colelawr@gmail.com>
2016-08-28 05:40:09 -0500

    Add dist

---
+++ dist/vendor.bundle.js
@@ -0,0 +1,8 @@
+webpackJsonp([2],[
+/* 0 */
+/***/ function(module, exports) {
+
+	eval("//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==");
+
+/***/ }
+]);
